9a80f000e7da20756ae78ef6b27d778c6ec6fb45
b1itz7 <b1itz@vip.qq.com>
2026-01-04 14:42:04 +0800

    implement DateConstants

--- common4j-core/src/main/java/cc/ashclaw/common4j/constants/DateConstants.java
+++ common4j-core/src/main/java/cc/ashclaw/common4j/constants/DateConstants.java
@@ -1,4 +1,13 @@
 package cc.ashclaw.common4j.constants;
 
 public class DateConstants {
+
+    public static final String YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd HH:mm:ss";
+
+    public static final String YYYY_MM_DD = "yyyy-MM-dd";
+
+    public static final String YYYY_MM = "yyyy-MM";
+
+    public static final String YYYY = "yyyy";
+
 }

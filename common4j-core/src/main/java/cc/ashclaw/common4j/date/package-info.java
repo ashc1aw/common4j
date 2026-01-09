47a8a2a0231eac752e792b82864a63a7c93bdc54
b1itz7 <b1itz@vip.qq.com>
2026-01-09 17:30:28 +0800

    docs: update comments

--- common4j-core/src/main/java/cc/ashclaw/common4j/date/package-info.java
+++ common4j-core/src/main/java/cc/ashclaw/common4j/date/package-info.java
@@ -6,5 +6,7 @@
  * Contains classes for date, time, and date-time manipulation, formatting, parsing, and conversion.
  * <p>
  * 包含用于日期、时间和日期时间操作、格式化、解析和转换的类。
+ *
+ * @since 1.0.0
  */
 package cc.ashclaw.common4j.date;
--- common4j-core/src/main/java/cc/ashclaw/common4j/model/package-info.java
+++ common4j-core/src/main/java/cc/ashclaw/common4j/model/package-info.java
@@ -1,1 +1,12 @@
+/**
+ * Model classes package.
+ * <p>
+ * 模型类包。
+ * <p>
+ * Contains common model classes for data representation.
+ * <p>
+ * 包含用于数据表示的通用模型类。
+ *
+ * @since 1.0.0
+ */
 package cc.ashclaw.common4j.model;

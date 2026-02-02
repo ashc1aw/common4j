95a8f4e6b65857ddf5e436f1cff3d4bcde5fc276
b1itz7 <b1itz@vip.qq.com>
2026-02-02 16:25:10 +0800

    feat: add util

---
+++ common4j-cache/src/main/java/cc/ashclaw/common4j/cache/util/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * Package containing cache utility classes.
+ * <p>
+ * 包含缓存工具类的包。
+ * <p>
+ * This package provides utility classes for common cache operations
+ * and distributed lock operations with simplified APIs.
+ * <p>
+ * 此包为常见的缓存操作和分布式锁操作提供工具类，具有简化的API。
+ *
+ * @author b1itz7
+ * @since 1.1.0
+ */
+package cc.ashclaw.common4j.cache.util;
